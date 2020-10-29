ecfc73e2330a57790ba9d3c26b7de45e25834c02
Tim Metzler <tim.metzler@h-brs.de>
2020-10-29 14:33:10 +0100

    Make exam kernel installable

--- exam_kernel/__main__.py
+++ exam_kernel/__main__.py
@@ -1,4 +1,49 @@
-from ipykernel.kernelapp import IPKernelApp
-from . import ExamKernel
+import argparse
+import os
+import sys
+from textwrap import dedent
+from jupyter_client.kernelspec import KernelSpecManager
 
-IPKernelApp.launch_instance(kernel_class=ExamKernel)
+def _is_root():
+    try:
+        return os.geteuid() == 0
+    except AttributeError:
+        return False
+
+class KernelManager:
+
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='ExamKernel manager.',
+        usage=dedent('''
+                python -m exam_kernel <command> [<args>]
+
+                Available sub commands are:
+                  install    install the kernel
+                  uninstall  uninstall the kernel
+            '''))
+
+        parser.add_argument('command', help='Subcommand to run')
+        parser.add_argument('--sys-prefix', action='store_true', help='If the kernel should be install to sys.prefix')
+        parser.add_argument('--user', action='store_true', help='If the kernel should be to the user space')
+
+        args = parser.parse_args(sys.argv[1:])
+        if not hasattr(self, args.command):
+            print('Unrecognized command')
+            parser.print_help()
+            exit(1)
+        if args.sys_prefix:
+            args.prefix = sys.prefix
+        if not args.prefix and not _is_root():
+            args.user = True
+        getattr(self, args.command)(prefix=args.prefix, user=args.user)
+
+    def install(self, prefix=None, user=True):
+        path = KernelSpecManager().install_kernel_spec(
+            os.path.dirname(__file__),
+            user=user, 
+            prefix=prefix
+        )
+        print(f'Install kernel to {path}')
+
+if __name__ == '__main__':
+    KernelManager()
--- setup.py
+++ setup.py
@@ -1,23 +1,29 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
 
 with open('README.rst') as f:
     readme = f.read()
 
+
 setup(
     name='exam_kernel',
     version='0.1',
-    packages=['exam_kernel'],
     description='Restricted python kernel for Jupyter',
     long_description=readme,
     author='Tim Metzler',
     author_email='tim.metzler@h-brs.de',
     url='https://github.com/DigiKlausur/exam_kernel',
-    install_requires=[
-        'jupyter_client', 'IPython', 'ipykernel'
-    ],
     classifiers=[
         'Intended Audience :: Developers',
         'License :: MIT',
         'Programming Language :: Python :: 3',
     ],
+    packages=find_packages(),
+    package_data={
+        '': ['*.json']
+    },
+    install_requires=[
+        'jupyter_client', 'IPython', 'ipykernel'
+    ],
+    
 )
