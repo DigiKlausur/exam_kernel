b12c5715e15e377395b20d7504e039b84b3c8d66
Tim Daniel Metzler <tim.metzler@h-brs.de>
2023-08-30 18:24:37 +0200

    Bump to 0.2.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 project = "Exam Kernel"
 copyright = "2023, Tim Metzler"
 author = "Tim Metzler"
-release = "0.1.2"
+release = "0.2.0"
 github_project_url = "https://github.com/Digiklausur/exam_kernel"
 
 # -- General configuration ---------------------------------------------------
